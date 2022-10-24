3d9842c266df1d8c5563f30bf782c5b7d3f6d855
Emma Maksimava <cielvibritania@gmail.com>
2022-10-24 11:38:17 +0200

    feat: add form behavior

--- src/app.js
+++ src/app.js
@@ -1,2 +1,36 @@
 import './style.css';
-console.log("Application is run");
+import { isValid } from './utils';
+
+const form = document.getElementById('form');
+const input = form.querySelector('#label-question');
+const btnSubmit = form.querySelector('#submit');
+
+
+form.addEventListener('submit', submitFormHeandler);
+input.addEventListener('input', () => {
+  btnSubmit.disabled = !isValid(input.value);
+})
+
+function submitFormHeandler(event){
+  event.preventDefault();
+
+  if(isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+
+    btnSubmit.disabled = true;
+    // Async request to server to save question
+
+    console.log('Question', question);
+
+    clearForm();
+  };
+}
+
+function clearForm() {
+  input.value = '';
+  input.className = '';
+  btnSubmit.disabled = true;
+}
